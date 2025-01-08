1a39384de8bf274a26665206abff66dea9087ee1
Ronak206 <Ronak241205@gmail.com>
2025-01-09 00:53:03 +0530

    Bit Strings

---
+++ Introductory Problems/Bit Strings.cpp
@@ -0,0 +1,57 @@
+/* 
+    Code by Ronak Rathod;
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define size(s)((int)(s).size())
+#define vi vector < int >
+#define vll vector < ll >
+#define vppi vector < pair < int, int >>
+#define vppl vector < pair < ll, ll >>
+#define forf(p, j, k) for (int p = j; p < k; p++)
+#define forb(p, j, k) for (int p = j; p > k; p--)
+#define all(x)(x).begin(), (x).end()
+#define allr(x)(x).rbegin(), (x).rend()
+#define sortf(x) sort(all(x))
+#define sortr(x) sort(allr(x))
+const int mod = 1e9 + 7;
+
+void aanya()
+{
+
+    int n;
+    cin>>n;
+    
+    ll ans = 1;
+    ll a = 2;
+   
+    while(n)
+    {
+        if(n&1) ans = (ans*a)%mod;
+        a = (a*a)%mod;
+        n >>= 1;
+    }
+    
+    cout<<ans<<endl;
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;;
+
+    while (t--)
+    {
+        aanya();
+    }
+
+    return 0;
+}
+//:)
