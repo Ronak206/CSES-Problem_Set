94732be422ccf4c963250e7b2331c20befad02b7
Ronak206 <Ronak241205@gmail.com>
2025-01-10 01:17:36 +0530

    Trailing Zeros

---
+++ Introductory Problems/Trailing Zeros.cpp
@@ -0,0 +1,54 @@
+/* 
+    Code by Ronak Rathod;
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define size(s)((int)(s).size())
+#define vi vector < int >
+#define vll vector < ll >
+#define vppi vector < pair < int, int >>
+#define vppl vector < pair < ll, ll >>
+#define forf(p, j, k) for (int p = j; p < k; p++)
+#define forb(p, j, k) for (int p = j; p > k; p--)
+#define all(x)(x).begin(), (x).end()
+#define allr(x)(x).rbegin(), (x).rend()
+#define sortf(x) sort(all(x))
+#define sortr(x) sort(allr(x))
+const int mod = 1e9 + 7;
+
+ll solve(ll n)
+{
+    if(n == 0) return 0;
+    
+    return n/5 + solve(n/5);
+}
+
+void aanya()
+{
+
+    ll n;
+    cin>>n;
+    
+    cout<<solve(n)<<endl;
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+
+    while (t--)
+    {
+        aanya();
+    }
+
+    return 0;
+}
+//:)
